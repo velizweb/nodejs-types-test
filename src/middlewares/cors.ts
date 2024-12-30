e8b98641202aedc306eeb578d51647f2dcd4c6d7
Alberto Veliz <velizweb@gmail.com>
2024-12-30 01:09:05 -0400

    Cors 1

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 
 export function setCorsHeaders(req: Request, res: Response, next: NextFunction) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
   }
